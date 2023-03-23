cc6ec06c40a14932957653117c822118963b1dc2
Harpica <harpy.ka@gmail.com>
2023-03-23 17:56:35 +0300

    init commit

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen text-red-300 bg-gradient-to-br from-gray-300 via-teal-700 to-gray-800'></div>
+    );
+}
+
+export default App;
